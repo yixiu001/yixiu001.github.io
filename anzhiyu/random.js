4925f3a822b7abdfd037f3c779454323dca40511
yixiu <mail@lvyabin.com>
2024-09-08 03:03:13 +0800

    Site updated: 2024-09-08 03:03:13

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/07/jisousoso/","2024/09/07/ssh/","2024/08/16/kxsw/","2024/08/06/serv00-re-ProxySite/","2024/08/03/segfault-faq/","2024/08/03/segfault/","2024/07/13/freeboundlessemail/","2024/07/12/serv00-re/","2024/07/11/meiquappleid-md/","2024/07/11/cloudnsdomainsre-md/","2024/07/11/uskgdomainsre-md/"];function toRandomPost(){
+var posts=["2024/09/08/serv00-singbox/","2024/09/07/jisousoso/","2024/09/07/ssh/","2024/08/16/kxsw/","2024/08/06/serv00-re-ProxySite/","2024/08/03/segfault-faq/","2024/08/03/segfault/","2024/07/13/freeboundlessemail/","2024/07/12/serv00-re/","2024/07/11/meiquappleid-md/","2024/07/11/cloudnsdomainsre-md/","2024/07/11/uskgdomainsre-md/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
